3d4a62fbac6c1656c810dcf3643ef5a66e76bdc3
Jeremy Smellie <jeremysmellie@gmail.com>
2016-07-05 22:22:43 -0400

    Summary:   * More random started things.  Also kinda started on a card system but didn't really do anything. :P

---
+++ Assets/Scripts/CardSystem/Base/eCardType.cs
@@ -0,0 +1,45 @@
+/* --------------------------
+ *
+ * eCardType.cs
+ *
+ * Description: Typesafe enum for the various card types
+ *
+ * Author: Jeremy Smellie
+ *
+ * Editors:
+ *
+ * 7/5/2016 - J & J
+ *
+ * All rights reserved.
+ *
+ * -------------------------- */
+
+#region Includes
+#region System Includes
+using System.Collections;
+#endregion
+
+#region Other Includes
+
+#endregion
+#endregion
+
+ namespace JJ.DungeonThing
+{
+	public class eCardType
+	{
+		#region Enum Values
+        #endregion
+
+        #region Constructor Methods
+        private eCardType()
+		{
+			
+		}
+		#endregion
+	
+		#region Private Methods
+		#endregion
+	}
+	
+}
